399939b00640633a2e4ee6a18c62dc327548663c
Travis Craft <tlcraft@users.noreply.github.com>
2024-04-23 16:57:16 -0600

    Setup base angular app.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err: any) => console.error(err));
