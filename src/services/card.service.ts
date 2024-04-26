b1e5c77fb5cd3097c356d57b8c5abd4f000bd8c1
Travis Craft <tlcraft@users.noreply.github.com>
2024-04-25 18:04:23 -0600

    Replace service with model.

--- src/services/card.service.ts
+++
@@ -1,8 +0,0 @@
-import { Inject, Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class CardService {
-  constructor(@Inject(String) public suit: string, @Inject(String) public value: string) {}
-}
