049e7a4fefd13451f3852a02669f1640a00daf53
Travis Craft <tlcraft@users.noreply.github.com>
2024-04-27 11:02:45 -0600

    Initial changes to inject game service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameService } from '../services/game.service';
+import { Card } from '../classes/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'card-game';
+
+  player1Card: Card = new Card('', '');
+  player2Card: Card = new Card('', '');
+
+  constructor(private gameService: GameService) {}
+
+  ngOnInit() {
+    this.gameService.startGame();
+    this.playRound();
+  }
+
+  playRound() {
+    const { player1Card, player2Card } = this.gameService.playRound();
+    if ( player1Card && player2Card) {
+      this.player1Card = player1Card;
+      this.player2Card = player2Card;
+    }
+  }
 }
